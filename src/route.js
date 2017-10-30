d30c7644b4b8d11a3d189b826bed8e8cb1e57f6e
zainilmin <zainilmin@gmail.com>
2017-10-30 09:32:24 -0500

    Update route.js

--- src/route.js
+++ src/route.js
@@ -17,56 +17,8 @@ export default class RouteComponent extends Component
                  <NavComponent />
                  <Route exact path="/" component={HomeComponent}/>
                  <Route path="/about" component={AboutComponent}/>
-                 <Route path="/topics" component={Topics}/>
                 </div>
            </Router>
         )
     }
 }
-
-//
-// const Home = () => (
-//   <div>
-//     <h2>Home</h2>
-//   </div>
-// )
-//
-// const About = () => (
-//   <div>
-//     <h2>About</h2>
-//   </div>
-// )
-//
- const Topics = ({ match }) => (
-   <div>
-     <h2>Topics</h2>
-     <ul>
-       <li>
-         <Link to={`${match.url}/rendering`}>
-           Rendering with React
-         </Link>
-       </li>
-       <li>
-         <Link to={`${match.url}/components`}>
-           Components
-         </Link>
-       </li>
-       <li>
-         <Link to={`${match.url}/props-v-state`}>
-           Props v. State
-         </Link>
-       </li>
-     </ul>
-
-     <Route path={`${match.url}/:topicId`} component={Topic}/>
-     <Route exact path={match.url} render={() => (
-       <h3>Please select a topic.</h3>
-     )}/>
-   </div>
- )
-
- const Topic = ({ match }) => (
-   <div>
-     <h3>{match.params.topicId}</h3>
-   </div>
- )
