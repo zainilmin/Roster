e5d86775bc41e219ef15e2f94a7b3e231474ca34
Zainil Maredia <zainil.maredia.df@renesas.com>
2018-01-26 11:28:00 -0600

    heroku deploy

--- server/client/src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const FETCH_USER = 'fetch_user';
-export const FETCH_STUDENTS = 'fetch_students';
-export const FETCH_STUDENT = 'fetch_student';
-export const NEW_STUDENT = 'new_student';
-export const UPDATE_STUDENT = 'update_student';
--- server/models/ClassSchedule.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } =  mongoose;
-
-const classScheduleSchema = new Schema({
-  classDate: Date,
-  classTime: String
-});
-
-mongoose.model('classschedule', classScheduleSchema);
